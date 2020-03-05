924329c8ea2c85240e8eb02fa185959d29fddd5f
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-03-05 19:44:59 +0530

    Added initial styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+import CurrencyRow from './currency-row.component';
 
+function App() {
+  return (
+    //? Empty fragment allows us to render more than one tag in 'return'
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div className='equals'>=</div>
+      <CurrencyRow />
+    </>
+  );
 }
 
 export default App;
