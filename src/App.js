b4b358d8f9912bd7ab3ecad0a250e618f9203776
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-03-05 20:21:50 +0530

    Added initial array of currencies via API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import CurrencyRow from './currency-row.component';
 
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
+
 function App() {
+  // This is run 2nd time to get all the currency options with keys
+  const [currencyOptions, setcurrencyOptions] = useState([]);
+
+  // This will only run the first time our app loads
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        setcurrencyOptions([data.base, ...Object.keys(data.rates)]);
+      });
+  }, []);
+
   return (
     //? Empty fragment allows us to render more than one tag in 'return'
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={currencyOptions} />
       <div className='equals'>=</div>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={currencyOptions} />
     </>
   );
 }
