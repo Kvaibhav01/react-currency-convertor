909c06f5fb10c636ad3c15d43cf1020be043d9e9
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-03-08 23:39:15 +0530

    Updated values of currencies

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 function App() {
   // This is run 2nd time to get all the currency options with keys
   const [currencyOptions, setcurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
 
   // This will only run the first time our app loads
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
+        // Get the first currency
+        const firstCurrency = Object.keys(data.rates)[0];
         setcurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
       });
   }, []);
 
@@ -22,9 +28,15 @@ function App() {
     //? Empty fragment allows us to render more than one tag in 'return'
     <>
       <h1>Convert</h1>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+      />
       <div className='equals'>=</div>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+      />
     </>
   );
 }
