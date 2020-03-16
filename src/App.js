748f0c35256f8b469f89c6b664b4518abd007015
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-03-16 20:12:24 +0530

    Added assets and design changes

--- src/App.js
+++ src/App.js
@@ -63,22 +63,56 @@ function App() {
   return (
     //? Empty fragment allows us to render more than one tag in 'return'
     <>
-      <h1>Convert</h1>
-      <CurrencyRow
-        currencyOptions={currencyOptions}
-        selectedCurrency={fromCurrency}
-        onChangeCurrency={event => setFromCurrency(event.target.value)}
-        onChangeAmount={handleFromAmountChange}
-        amount={fromAmount}
-      />
-      <div className='equals'>=</div>
-      <CurrencyRow
-        currencyOptions={currencyOptions}
-        selectedCurrency={toCurrency}
-        onChangeCurrency={event => setToCurrency(event.target.value)}
-        onChangeAmount={handleToAmountChange}
-        amount={toAmount}
-      />
+      <div className='container'>
+        <h1>currency convertor</h1>
+        {/* Icon here */}
+        <p className='desc'>
+          Enter the currency value, pick a type and see the <br /> conversion!
+          Or try other way round
+          <span role='img' aria-label='smiling emoji'>
+            😉
+          </span>
+        </p>
+        <div className='theme-toggle'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='44'
+            height='43.8'
+            viewBox='0 0 44 43.8'
+          >
+            <g id='ic_wb_sunny_48px' transform='translate(-2 -1.1)'>
+              <path
+                id='Path_1'
+                data-name='Path 1'
+                d='M13.51,9.69,9.93,6.1,7.1,8.93l3.59,3.59,2.82-2.83ZM8,21H2v4H8ZM26,1.1H22V7h4ZM40.9,8.93,38.07,6.1,34.48,9.69l2.83,2.83L40.9,8.93ZM34.49,36.31l3.59,3.59,2.83-2.83-3.59-3.59-2.83,2.83ZM40,21v4h6V21ZM24,11A12,12,0,1,0,36,23,12,12,0,0,0,24,11ZM22,44.9h4V39H22ZM7.1,37.07,9.93,39.9l3.59-3.59-2.83-2.83L7.1,37.07Z'
+                fill='#ff8906'
+              />
+            </g>
+          </svg>
+        </div>
+        <CurrencyRow
+          currencyOptions={currencyOptions}
+          selectedCurrency={fromCurrency}
+          onChangeCurrency={event => setFromCurrency(event.target.value)}
+          onChangeAmount={handleFromAmountChange}
+          amount={fromAmount}
+        />
+        <div className='equals'>equals</div>
+        <CurrencyRow
+          currencyOptions={currencyOptions}
+          selectedCurrency={toCurrency}
+          onChangeCurrency={event => setToCurrency(event.target.value)}
+          onChangeAmount={handleToAmountChange}
+          amount={toAmount}
+        />
+        <p>
+          Made with React by Vaibhav Khulbe. Data sourced from&nbsp;
+          <a href='https://exchangeratesapi.io/' target='blank'>
+            exchangeratesapi.io
+          </a>
+          .
+        </p>
+      </div>
     </>
   );
 }
--- src/currency-row.component.jsx
+++ src/currency-row.component.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 export default function CurrencyRow(props) {
-  const { currencyOptions, selectedCurrency, onChangeCurrency, onChangeAmount,amount } = props;
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount
+  } = props;
   return (
-    <div>
-      <input type='number' className='input' value={amount} onChange={onChangeAmount}/>
+    <div className='inputArea'>
+      <p className='inputLabel'>Enter 'from' currency</p>
+      <input
+        type='number'
+        className='input'
+        value={amount}
+        onChange={onChangeAmount}
+      />
+
+      <p className='inputLabel'>Pick currency</p>
       <select value={selectedCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map(option => (
           <option key={option} value={option}>
